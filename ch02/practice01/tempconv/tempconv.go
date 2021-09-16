1fdc5a27a6d1895b551cda442c341cdfc7186d29
jeterdx <gomitamu@gmail.com>
2021-09-16 12:41:54 +0900

    Added tempconv execution and mod.go to enable an import of package.

--- ch02/practice01/tempconv/tempconv.go
+++ ch02/practice01/tempconv/tempconv.go
@@ -9,11 +9,11 @@ type Fahrenheit float64
 type Kelvin float64
 
 const (
-  AbsolutezeroC Celsius = -273.15
-  BoilingC Celsius = 100.0
-  FeezingC Celsius　= 0.0
+	AbsoluteZeroC Celsius = -273.15
+	BoilingC      Celsius = 100.0
+	FreezingC     Celsius = 0.0
 )
 
-func (c Celsius) String() string {return fmt.Sprintf("%g°C", c)}
-func (f Fahrenheit) String() string {return fmt.Sprintf("%g°F", f)}
-func (k Kelvin) String() string {return fmt.Sprintf("%g°K", k)}
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
