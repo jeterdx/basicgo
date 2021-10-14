dbee0e18aea3bc4786d6e0228a08ad75b0270f51
jeterdx <gomitamu@gmail.com>
2021-10-14 22:57:30 +0900

    fixed practice01 of chap03

--- ch03/practice01/svg.go
+++ ch03/practice01/svg.go
@@ -26,10 +26,22 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+			if isValid([]float64{ax, ay, bx, by, cx, cy, dx, dy}) {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+			}
 		}
 	}
 	fmt.Println("</svg>")
+}
+
+//無限大の場合にfalseを返す判定。
+func isValid(points []float64) bool {
+	for _, value := range points {
+		if math.IsInf(value, 0) {
+			return false
+		}
+	}
+	return true
 }
 
 func corner(i, j int) (float64, float64) { // ます目 (i,j) のかどの点 (x,y) を見つける。
