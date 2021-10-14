123265d9e10318338079a44e3d10bb38faf6b3ce
jeterdx <gomitamu@gmail.com>
2021-10-14 23:02:56 +0900

    modified a math function of practice02 of chap03

--- ch03/practice02/svg.go
+++ ch03/practice02/svg.go
@@ -42,6 +42,6 @@ func corner(i, j int) (float64, float64) { // ます目 (i,j) のかどの点 (x
 }
 
 func f(x, y float64) float64 {
-	r := math.Hypot(x, y) // (0,0) からの距離
-	return math.Sin(r) / r
+	r := math.Nextafter(x, y) // (0,0) からの距離
+	return math.Cos(r) / r
 }
