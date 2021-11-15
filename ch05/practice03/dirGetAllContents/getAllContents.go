a835b8cdb68ba2b6eaa805124018b147d551e12a
jeterdx <gomitamu@gmail.com>
2021-11-15 12:04:43 +0900

    Revised a source of practice03 of ch05

--- ch05/practice03/dirGetAllContents/getAllContents.go
+++ ch05/practice03/dirGetAllContents/getAllContents.go
@@ -16,15 +16,26 @@ func main() {
 	for _, content := range visit(nil, doc) { //visitの返り値の<a href>に該当するvalueの[]stirngをrangeで回してる。
 		fmt.Println(content)
 	}
+	//fmt.Println(visit(nil, doc))
 
 }
 
 func visit(contents []string, n *html.Node) []string {
-	if n.Type == html.TextNode { //nodeのタイプがテキストノードのDataをappendしていく
+	if n.Type == html.TextNode { //nodeのタイプがテキストノードのDataをappendして
 		contents = append(contents, n.Data)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling { //今見ているnodeに子nodeがnilになるまで、子nodeをvisitに渡して再帰する。そして隣接するnodeをcにセットして、tree構造を下にたどっていく。
 		contents = visit(contents, c)
 	}
-	return contents
+	//改行だけの要素とかが邪魔なので取り除いて新しいリストを返す
+	var contentsDumped []string
+	for _, content := range contents {
+		for _, r := range content {
+			if r != 10 {
+				contentsDumped = append(contentsDumped, content)
+				break
+			}
+		}
+	}
+	return contentsDumped
 }
